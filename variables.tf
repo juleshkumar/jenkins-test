e962b70d6fb90208b78ac8a36897cdef22063f85
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-03-14 22:13:05 +0530

    ami change

--- variables.tf
+++ variables.tf
@@ -20,7 +20,7 @@ variable "aws_region" {
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID"
-   default     = "ami-0f5ee92e2d63afc18"
+   default     = "ami-03bb6d83c60fc5f7c"
 }
 
 variable "instance_type" {
