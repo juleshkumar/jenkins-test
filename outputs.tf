88b8d0e696070340da959f4c28b78ee073b29383
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-03-15 17:19:56 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,23 @@
-output "vpcs" {
-  description = "VPC Outputs"
-  value       = { for vpc in aws_vpc.main : vpc.tags.Name => { "cidr_block" : vpc.cidr_block, "id" : vpc.id } }
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_ids" {
+  value = aws_subnet.public[*].id
+}
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "private_route_table_ids" {
+  value = aws_route_table.private[*].id
+}
+
+output "igw_id" {
+  value = aws_internet_gateway.igw.id
+}
+
+output "public_route_table_id" {
+  value = aws_default_route_table.default.id
 }
