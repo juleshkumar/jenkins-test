633fcf4930608cee5cb56b66bbc9b508204c050f
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-03-15 23:18:55 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,13 +3,23 @@ output "id" {
   description = "VPC ID"
 }
 
-output "public_subnet_ids" {
-  value       = aws_subnet.public.*.id
+output "public1_subnet_ids" {
+  value       = aws_subnet.public1.id
   description = "List of public subnet IDs"
 }
 
-output "private_subnet_ids" {
-  value       = aws_subnet.private.*.id
+output "public2_subnet_ids" {
+  value       = aws_subnet.public2.id
+  description = "List of public subnet IDs"
+}
+
+output "private1_subnet_ids" {
+  value       = aws_subnet.private1.id
+  description = "List of private subnet IDs"
+}
+
+output "private2_subnet_ids" {
+  value       = aws_subnet.private2.id
   description = "List of private subnet IDs"
 }
 
