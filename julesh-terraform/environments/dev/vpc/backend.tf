38180212eef6a8bb04a560a98913926a0d5111ac
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-05-02 22:16:11 +0530

    Update backend.tf

--- julesh-terraform/environments/dev/vpc/backend.tf
+++ julesh-terraform/environments/dev/vpc/backend.tf
@@ -3,8 +3,9 @@ terraform {
     bucket   = "terrafrom-test-to-delete-bucket"
     key      = "backend/vpc"
     region   = "ap-south-1"
-    assume_role {
-      role_arn = "arn:aws:iam::471112548391:role/terraform-role"
-    }
+  }
+  
+  assume_role {
+    role_arn = "arn:aws:iam::471112548391:role/terraform-role"
   }
 }
