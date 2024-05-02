ee20b579f18b6e0af9d92cc96a5d07302f99477c
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-05-02 22:23:08 +0530

    Update backend.tf

--- julesh-terraform/environments/dev/vpc/backend.tf
+++ julesh-terraform/environments/dev/vpc/backend.tf
@@ -5,10 +5,3 @@ terraform {
     region   = "ap-south-1"
   }
 }
-
-provider "aws" {
-  region = "ap-south-1"
-  assume_role {
-    role_arn = "arn:aws:iam::471112548391:role/terraform-role"
-  }
-}
