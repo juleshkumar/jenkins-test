4e9143d9f797e48787863f6553ac1c8c25a51bad
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-05-03 15:56:33 +0530

    Update backend.tf

--- julesh-terraform/environments/dev/ec2-jumpbox/backend.tf
+++ julesh-terraform/environments/dev/ec2-jumpbox/backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket     = "terrafrom-test-to-delete-bucket"
     key        = "backend/jumpbox"
-    region     = "apa-south-1"
-    role_arn   = "arn:aws:iam::471112548391:role/terraform-role"
+    region     = "ap-south-1"
   }
 }
