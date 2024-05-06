db4ca88be44d72c3e858d2ce003e9590431744a7
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-05-06 17:15:02 +0530

    Update backend.tf

--- julesh-terraform/environments/dev/kms/backend.tf
+++ julesh-terraform/environments/dev/kms/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket     = "terrafrom-test-to-delete-bucket"
     key        = "backend/kms"
-    region     = "us-east-1"
+    region     = "ap-south-1"
   }
 }
 
