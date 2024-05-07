7fbb4d8622640b007f5c7018ba1d43b36b3c3e92
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-05-08 01:23:44 +0530

    Update backend.tf

--- julesh-terraform/environments/dev/efs/backend.tf
+++ julesh-terraform/environments/dev/efs/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket     = "terrafrom-test-to-delete-bucket "
+    bucket     = "terrafrom-test-to-delete-bucket"
     key        = "backend/efs"
     region     = "ap-south-1"
   }
