87caaba89867791053d15f296f63926a1eb4965e
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-05-20 22:50:15 +0530

    Update main.tf

--- julesh-terraform/modules/nodegroup/main.tf
+++ julesh-terraform/modules/nodegroup/main.tf
@@ -256,5 +256,39 @@ resource "aws_eks_node_group" "on_demand" {
     aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
+resource "aws_eks_addon" "coredns" {
+  cluster_name                = aws_eks_cluster.eks.name
+  addon_name                  = "coredns"
+  addon_version               = "v1.10.1-eksbuild.4"
+  resolve_conflicts_on_update = "PRESERVE" # or "OVERRIDE" depending on your preference
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name                = aws_eks_cluster.eks.name
+  addon_name                  = "kube-proxy"
+  addon_version               = "v1.28.2-eksbuild.2"
+  resolve_conflicts_on_update = "PRESERVE" # or "OVERRIDE" depending on your preference
+}
+
+resource "aws_eks_addon" "ebs_csi_driver" {
+  cluster_name                = aws_eks_cluster.eks.name
+  addon_name                  = "ebs-csi-driver"
+  addon_version               = "v1.30.0-eksbuild.1"
+  resolve_conflicts_on_update = "PRESERVE" # or "OVERRIDE" depending on your preference
+}
+
+resource "aws_eks_addon" "efs_csi_driver" {
+  cluster_name                = aws_eks_cluster.eks.name
+  addon_name                  = "efs-csi-driver"
+  addon_version               = "v2.0.2-eksbuild.1"
+  resolve_conflicts_on_update = "PRESERVE" # or "OVERRIDE" depending on your preference
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name                = aws_eks_cluster.eks.name
+  addon_name                  = "vpc-cni"
+  addon_version               = "v1.15.1-eksbuild.1"
+  resolve_conflicts_on_update = "PRESERVE" # or "OVERRIDE" depending on your preference
+}
 
 
