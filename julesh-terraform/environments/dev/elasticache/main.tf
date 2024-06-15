bcc1b88f4528e9bcf3212761d9d2dd4400504032
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-06-15 11:56:27 +0530

    Update main.tf

--- julesh-terraform/environments/dev/elasticache/main.tf
+++ julesh-terraform/environments/dev/elasticache/main.tf
@@ -1,10 +1,21 @@
 module "elasticache" {
-  source                 = "../../../modules/elasticache"
-  redis-cluster          = var.redis-cluster
-  redis-engine           = var.redis-engine
-  redis-engine-version   = var.redis-engine-version
-  redis-node-type        = var.redis-node-type
-  num-cache-nodes        = var.num-cache-nodes
-  parameter-group-family = var.parameter-group-family
-  replication-id         = var.replication-id
+  source               = "../../../modules/elasticache"
+  redis-cluster        = var.redis-cluster
+  redis-engine         = var.redis-engine
+  redis-engine-version = var.redis-engine-version
+  redis-node-type      = var.redis-node-type
+  #  num-cache-nodes        = var.num-cache-nodes
+  parameter-group-family  = var.parameter-group-family
+  replication-id          = var.replication-id
+  auth_token              = var.auth_token
+  num-node-groups         = var.num-node-groups
+  replicas-per-node-group = var.replicas-per-node-group
+  rest_encryption         = var.rest_encryption
+  environment             = var.environment
+  ec_key                  = var.ec_key
+  redis_password          = var.redis_password
+  kms_key                 = var.kms_key
+  redis-user-id           = var.redis-user-id
+  redis-user-name         = var.redis-user-name
+  transit_encryption_enabled = var.transit_encryption_enabled
 }
