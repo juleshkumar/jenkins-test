cf492f98cf1dbccc6c1e90848772f95e954de813
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-06-15 11:50:09 +0530

    Update main.tf

--- julesh-terraform/modules/elasticache/main.tf
+++ julesh-terraform/modules/elasticache/main.tf
@@ -23,17 +23,6 @@ resource "aws_elasticache_parameter_group" "redis_parameter_group" {
   family      = var.parameter-group-family
   description = "Custom parameter group for ElastiCache Redis"
 
-  parameter {
-    name  = "hash-max-ziplist-entries"
-    value = "512"
-  }
-
-  parameter {
-    name  = "hash-max-ziplist-value"
-    value = "64"
-  }
-
-  # Add more parameters as needed
 }
 
 resource "aws_security_group" "elasticache_security_group" {
@@ -63,23 +52,54 @@ resource "aws_elasticache_subnet_group" "eccr" {
   subnet_ids = data.terraform_remote_state.vpc_state.outputs.private_subnet_ids
 }
 
+resource "aws_elasticache_user" "redis_user" {
+  user_id   = var.redis-user-id
+  user_name = var.redis-user-name
+  engine    = var.redis-engine
+  passwords = [var.redis_password]  
+  access_string = "on ~* +@all"  # Adjust access string as needed
+
+  tags = {
+    Name = "redis-user"
+    Environment = var.environment
+  }
+}
+
+resource "aws_elasticache_user_group" "redis_user_group" {
+  user_group_id = "user-group-redis"
+  engine        = var.redis-engine
+  user_ids      = [aws_elasticache_user.redis_user.user_id]
+
+  tags = {
+    Name = "redis-user-group"
+    Environment = var.environment
+  }
+}
+
+
 resource "aws_elasticache_replication_group" "erg" {
-  replication_group_id        = var.replication-id
-  description                 = "elasticache replication group"
+  replication_group_id = var.replication-id
+  description          = "Elasticache replication group"
   engine               = var.redis-engine
   engine_version       = var.redis-engine-version
   node_type            = var.redis-node-type
-  num_node_groups = var.num-cache-nodes
-  subnet_group_name = aws_elasticache_subnet_group.eccr.name
-  parameter_group_name = aws_elasticache_parameter_group.redis_parameter_group.name
-  security_group_ids = [aws_security_group.elasticache_security_group.id]
-  port = 6379
-  at_rest_encryption_enabled = true
-  kms_key_id = data.terraform_remote_state.kms.outputs.key_arn
-  transit_encryption_enabled = false
-
+  #  num_cache_clusters            = var.num-cache-nodes
+  num_node_groups            = var.num-node-groups # Ensure this is set correctly for cluster mode
+  replicas_per_node_group    = var.replicas-per-node-group
+  subnet_group_name          = aws_elasticache_subnet_group.eccr.name
+  parameter_group_name       = aws_elasticache_parameter_group.redis_parameter_group.name
+  security_group_ids         = [aws_security_group.elasticache_security_group.id]
+  port                       = 6379
+  at_rest_encryption_enabled = var.rest_encryption
+  kms_key_id                 = data.terraform_remote_state.kms.outputs.key_arn
+  transit_encryption_enabled = var.transit_encryption_enabled
+#  transit_encryption_mode    = "preferred"
+#  auth_token                 = var.auth_token
+  automatic_failover_enabled = true
+  user_group_ids = [aws_elasticache_user_group.redis_user_group.user_group_id]
   tags = {
-    Name = var.redis-cluster
+    Name        = var.redis-cluster
+    Environment = var.environment
   }
 }
 
