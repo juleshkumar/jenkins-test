ec56942279134f960ef66b78d37e8f5acf4013d7
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-06-17 10:56:30 +0530

    Update outputs.tf

--- julesh-terraform/modules/elasticache/outputs.tf
+++ julesh-terraform/modules/elasticache/outputs.tf
@@ -18,13 +18,8 @@ output "redis_cluster_endpoint" {
   description = "The endpoint of the Redis cluster"
   value       = aws_elasticache_replication_group.erg.primary_endpoint_address
 }
- 
+
 output "redis_cluster_configuration_endpoint" {
   description = "The configuration endpoint of the Redis cluster"
   value       = aws_elasticache_replication_group.erg.configuration_endpoint_address
 }
-
-
-
-
-
