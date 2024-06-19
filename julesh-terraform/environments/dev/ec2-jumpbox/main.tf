438df0c1ba24eaac43c8a6972194a8e9a839dac4
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-06-19 16:46:18 +0530

    Update main.tf

--- julesh-terraform/environments/dev/ec2-jumpbox/main.tf
+++ julesh-terraform/environments/dev/ec2-jumpbox/main.tf
@@ -1,7 +1,14 @@
 module "jumpbox" {
   source            = "../../../modules/ec2-jumpbox"
   ami               = var.ami
+  public_key_file   = var.public_key_file
   ec2_instance_type = var.ec2_instance_type
   ec2_key_name      = var.ec2_key_name
-  js_user = var.js_user
+  js_user           = var.js_user
+  environment       = var.environment
+  role              = var.role
+  region            = var.region
+  backend_bucket    = var.backend_bucket
+  jumpbox_key       = var.jumpbox_key
+  vpc_key = var.vpc_key
 }
