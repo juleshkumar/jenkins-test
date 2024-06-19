21711346ba24973aace4417c3426ca67c9ca3bf9
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-06-19 16:09:57 +0530

    Update backend.tf

--- julesh-terraform/environments/dev/ec2-jumpbox/backend.tf
+++ julesh-terraform/environments/dev/ec2-jumpbox/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket     = "terrafrom-test-to-delete-bucket"
+    bucket     = var.backend_bucket
     key        = "backend/jumpbox"
-    region     = "ap-south-1"
+    region     = var.region
   }
 }
