3c786c5074038e78f4a23d61a2868a20e171bf70
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-06-19 17:31:14 +0530

    Update main.tf

--- julesh-terraform/environments/dev/efs/main.tf
+++ julesh-terraform/environments/dev/efs/main.tf
@@ -2,4 +2,6 @@ module "efs" {
   source             = "../../../modules/efs"
   cluster-name       = var.cluster-name
   efs-security-group = var.efs-security-group
+  backend_bucket     = var.backend_bucket
+  region             = var.region
 }
