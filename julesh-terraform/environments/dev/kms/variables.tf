99aaad1d3067eaeb4676c658314c77cfb9c93929
juleshkumar <154207267+juleshkumar@users.noreply.github.com>
2024-06-20 18:32:49 +0530

    Update variables.tf

--- julesh-terraform/environments/dev/kms/variables.tf
+++ julesh-terraform/environments/dev/kms/variables.tf
@@ -2,4 +2,10 @@ variable "kms_key_name" {
   type        = string
   description = "(optional) describe your variable"
 }
+variable "region" {
+  type = string
+}
 
+variable "backend_bucket" {
+  type = string
+}
